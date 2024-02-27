abdb853a9e48ff5d8fa0e781b94804d226c4382b
Deborah McVey <deborah.mcvey@gmail.com>
2024-02-27 17:43:18 -0500

    completed video Fetching Events with Pagination

--- src/app/features/events/events.component.ts
+++ src/app/features/events/events.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../../core/services/event.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-events',
@@ -13,10 +14,14 @@ export class EventsComponent implements OnInit {
   totalPages:number = 0;
   events:Event[] = []
 
-  constructor(private eventService:EventService) {}
+  constructor(private eventService:EventService, private router:Router, private route:ActivatedRoute) {}
 
   ngOnInit(): void {
-      this.loadEvents(this.currentPage)
+      // this.loadEvents(this.currentPage)
+      this.route.queryParams.subscribe(params => {
+        const page = params['page'] ? Number(params['page']) : 1
+        this.loadEvents(page)
+      })
   }
 
   loadEvents(page:number) {
@@ -34,13 +39,23 @@ export class EventsComponent implements OnInit {
 
   nextPage() {
     if(this.currentPage < this.totalPages) {
-      this.loadEvents(this.currentPage + 1)
+      //this.loadEvents(this.currentPage + 1)
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: { page: this.currentPage + 1 },
+        queryParamsHandling: 'merge'
+    })
     }
   }
 
   previousPage() {
     if(this.currentPage > 1) {
-      this.loadEvents(this.currentPage - 1)
+      //this.loadEvents(this.currentPage - 1)
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: { page: this.currentPage - 1 },
+        queryParamsHandling: 'merge'
+    })
     }
   }
 }
--- src/app/shared/components/events/event/event.component.ts
+++ src/app/shared/components/events/event/event.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './event.component.html',
   styleUrl: './event.component.scss'
 })
 export class EventComponent {
-
+  @Input({required:true}) event: Event = new Event({})
 }
