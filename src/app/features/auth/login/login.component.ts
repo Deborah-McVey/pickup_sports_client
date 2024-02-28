e7ac0a4afcff215d37bf9d4be93a1826147f2df4
Deborah McVey <deborah.mcvey@gmail.com>
2024-02-27 20:17:17 -0500

    15:33 into Building Login Functionality video

---
+++ src/app/features/auth/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthenticationService } from '../../../core/services/authentication.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm: FormGroup = new FormGroup({
+    user_name: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  })
+
+  constructor(private authService:AuthenticationService, private router:Router) {}
+
+  login() {
+    if(this.loginForm.valid) {
+      const user_name = this.loginForm.value.user_name;
+      const password = this.loginForm.value.password;
+
+      this.authService.login(user_name, password).subscribe({
+        next: (res:any) => {
+          console.log(res);
+          this.router.navigate(['/'])
+        },
+        error: (error:any) => {
+          console.log("Error when logging", error)
+        }
+      })
+    }
+  }
+}
